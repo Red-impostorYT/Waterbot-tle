f6023c44d7b4d0e2a1cd40355d2d686067b5e82f
Red_impostorYT <130860976+Red-impostorYT@users.noreply.github.com>
2024-07-09 18:13:24 -0600

    Add files via upload

---
+++ waterbot-tle.py
@@ -0,0 +1,29 @@
+import discord
+import time
+from datetime import datetime
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print('------')
+
+    async def on_message(self, message):
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('!water'):
+            await message.reply('Starting...', mention_author=True)
+            time.sleep(5)
+            while True:
+                    irl_time = time.strftime("%I:%M %p")
+                    await message.reply('Drink Water!', mention_author=True)
+                    time.sleep(5)
+                    await message.reply(irl_time, mention_author=True)
+                    time.sleep(9000) #2.5 Hours
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+
+client.run('token') #Your bot token (Don't share with people!!)
